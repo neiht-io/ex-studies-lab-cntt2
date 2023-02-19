e55254fabde6c5147674ff88cda6501219431954
khac thien nguyen <124896934+neiht-io@users.noreply.github.com>
2023-02-19 13:07:18 +0700

    Add files via upload

---
+++ VariableInJava.java
@@ -0,0 +1,15 @@
+public class VariableInJava {
+
+    public static void main(String[] args) {
+        String name;
+        float height;
+        int age;
+        name = "Chau Kter";
+        height = 1.7f;
+        age = 21;
+        System.out.println(name);
+        System.out.println(height);
+        System.out.println(age);
+    }
+
+}
